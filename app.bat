003c443ce3e46002a1bafd57b56875b2557ed7ae
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2026-01-19 15:53:00 +0800

    feat: automation execution

---
+++ app.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Downloading...
+
+REM
+cd /d "D:\Datalizer-Automation"
+
+REM
+python app.py
+
+echo Download berhasil.
+pause
---
+++ sync.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Synchronize...
+
+REM 
+cd /d "D:\Datalizer-Automation"
+
+REM
+python sync.py
+
+echo Selesai.
+pause
